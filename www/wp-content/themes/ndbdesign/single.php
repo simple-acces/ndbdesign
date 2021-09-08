092e68e0543aa035c7bda0f5a48e23f3c87993a6
RonanDrouglazet <ronan.drouglazet@teads.tv>
2021-09-08 23:20:41 +0200

    single post

--- www/wp-content/themes/ndbdesign/single.php
+++ www/wp-content/themes/ndbdesign/single.php
@@ -16,12 +16,12 @@ while ( have_posts() ) :
 	the_post();
 
 	get_template_part( 'template-parts/content/content-single' );
-
+	/*
 	if ( is_attachment() ) {
 		// Parent post navigation.
 		the_post_navigation(
 			array(
-				/* translators: %s: Parent post link. */
+				// translators: %s: Parent post link.
 				'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentytwentyone' ), '%title' ),
 			)
 		);
@@ -31,18 +31,18 @@ while ( have_posts() ) :
 	if ( comments_open() || get_comments_number() ) {
 		comments_template();
 	}
-
+	*/
 	// Previous/next post navigation.
 	$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
 	$twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
 
-	$twentytwentyone_next_label     = esc_html__( 'Next post', 'twentytwentyone' );
-	$twentytwentyone_previous_label = esc_html__( 'Previous post', 'twentytwentyone' );
-
+	$twentytwentyone_next_label     = esc_html__( 'Suivant', 'twentytwentyone' );
+	$twentytwentyone_previous_label = esc_html__( 'Précédent', 'twentytwentyone' );
+	
 	the_post_navigation(
 		array(
-			'next_text' => '<p class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</p><p class="post-title">%title</p>',
-			'prev_text' => '<p class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</p><p class="post-title">%title</p>',
+			'next_text' => '<button class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</button>',
+			'prev_text' => '<button class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</button>',
 		)
 	);
 endwhile; // End of the loop.
