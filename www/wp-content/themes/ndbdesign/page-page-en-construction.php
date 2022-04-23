1cb8d4ed247081a05b2f19c14fca784ed6ec3faa
RonanDrouglazet <ronan.drouglazet@teads.tv>
2022-04-23 21:34:47 +0200

    update page en construction

--- www/wp-content/themes/ndbdesign/page-page-en-construction.php
+++ www/wp-content/themes/ndbdesign/page-page-en-construction.php
@@ -4,19 +4,18 @@ get_header();
 
    ?>
         <div class="content-area">
-        <h1>Page en construction, demain sera plus beau !</h1>
-        <div class="image">
         <?php if ( has_post_thumbnail() ) :
+         ?> <h1>Page en construction, demain sera plus beau !</h1> <?php
 			the_post_thumbnail( 'full' );
         else:
          ?>   
+            <h1>Page introuvable</h1>
             <div class="image">
                <img alt="image accueil" style="max-width:100%" src="/wp-content/themes/ndbdesign/assets/images/home.jpeg" />
             </div>
          <?php
 		   endif; 
          ?>
-        </div>
         <br/><br/>
    </div>
      <?php
